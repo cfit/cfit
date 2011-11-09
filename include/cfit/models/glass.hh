e8a352b21109e754cc77a14e0b39c7ec9ffbd8dc
jordix <cfit@lavabit.com>
2011-11-09 17:13:55 +0100

    Added implementation of the generalized LASS resonance.

---
+++ include/cfit/models/glass.hh
@@ -0,0 +1,46 @@
+#ifndef __GLASS_HH__
+#define __GLASS_HH__
+
+#include <cfit/parameter.hh>
+#include <cfit/resonance.hh>
+
+class PhaseSpace;
+
+// Generalized Lass resonance.
+class GLass : public Resonance
+{
+public:
+  template <class T>
+  GLass( const T&         resoA, const T&         resoB,
+         const Parameter& mass , const Parameter& width,
+         const Parameter& r    ,
+         const Parameter& lassR, const Parameter& lassB,
+         const Parameter& phiR , const Parameter& phiB ,
+         const Parameter& lassr, const Parameter& lassa,
+         const int&       l      )
+    : Resonance( resoA, resoB, mass, width, r, l )
+    {
+      push( lassR );
+      push( lassB );
+      push( phiR  );
+      push( phiB  );
+      push( lassr );
+      push( lassa );
+    }
+
+  GLass( const GLass& right )
+    : Resonance( right )
+    {}
+
+  double lassR() const { return getPar( 0 ); }
+  double lassB() const { return getPar( 1 ); }
+  double phiR()  const { return getPar( 2 ); }
+  double phiB()  const { return getPar( 3 ); }
+  double lassa() const { return getPar( 4 ); }
+  double lassr() const { return getPar( 5 ); }
+
+  std::complex< double > propagator( const PhaseSpace& ps, const double& mSqAB ) const;
+  GLass*                 copy()                                                  const;
+};
+
+#endif
---
+++ src/models/glass.cc
@@ -0,0 +1,31 @@
+
+#include <complex>
+#include <cfit/phasespace.hh>
+#include <cfit/models/glass.hh>
+
+
+std::complex< double > GLass::propagator( const PhaseSpace& ps, const double& mSqAB ) const
+{
+  const std::complex< double > I( 0., 1. );
+
+  double qAB   = q  ( ps, mSqAB );
+  double qSqAB = qSq( ps, mSqAB );
+
+  double qCotDeltaB = 1. / lassa() + lassr() * qSqAB / 2.;
+  double cotDeltaB  = qCotDeltaB / qAB;
+
+  std::complex< double > rTerm = lassR() * std::exp( I * phiR() + 2. * I * phiB() );
+  rTerm *= ( qCotDeltaB + I * qAB ) / ( qCotDeltaB - I * qAB );
+  rTerm *= m() * width() / ( mSq() - mSqAB - I * m() * runningWidth( ps, mSqAB ) );
+
+  std::complex< double > bTerm = lassB() * mSqAB / 2. * std::exp( I * phiB() );
+  bTerm *= ( std::cos( phiB() ) + std::sin( phiB() ) * cotDeltaB ) / ( qCotDeltaB - I * qAB );
+
+  return rTerm + bTerm;
+}
+
+
+GLass* GLass::copy() const
+{
+  return new GLass( *this );
+}
