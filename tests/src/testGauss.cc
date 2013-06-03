4c9d53b8fd44be2fa72f0b7fa683b90237fa7b1f
jordix <cfit@lavabit.com>
2013-06-03 11:53:00 +0200

    Make the example gaussian fit generate toy events if requested.

--- tests/src/testGauss.cc
+++ tests/src/testGauss.cc
@@ -2,27 +2,23 @@
 #include <fstream>
 
 #include <cfit/parameter.hh>
-#include <cfit/parameterexpr.hh>
 #include <cfit/variable.hh>
 #include <cfit/dataset.hh>
 #include <cfit/chi2.hh>
 #include <cfit/pdf.hh>
 
-#include <cfit/operation.hh>
-
 #include <cfit/models/gauss.hh>
 
 #include <Minuit/MnPrint.h>
 
-#ifdef MPI_ON
-#include <mpi.h>
-#endif
-
-#define MIN   ( -10. )
-#define MAX   (  10. )
-#define NBIN  ( 100. )
+#define MIN   ( -10.0   )
+#define MAX   (  10.0   )
+#define NBIN  ( 100.0   )
+#define NEVT  (   1.0e6 )
 
 #define RANGE ( MAX - MIN )
+#define AREA  ( RANGE * NEVT / NBIN ) // Expected value of the area.
+
 
 // Auxiliary functions to compute the bin number from a point, the center of
 //    a bin, the (approximate) Poisson error, and to read a data file.
@@ -34,19 +30,28 @@ void   readData ( std::string fileName, Dataset& data )
   double x;
   double y[ 100 ];
 
+  // Initialize the bin contents to zero.
   for ( int b = 0; b < 100; b++ )
     y[ b ] = 0;
 
+  // Open the input file.
   std::ifstream file( fileName.c_str() );
+  if ( ! file )
+    throw std::exception();
+
+  // Read the input file.
   while ( file >> x )
     y[ bin( x ) ]++;
+
+  // Close the input file.
   file.close();
 
+  // Fill the dataset with the histogram.
   for ( int b = 0; b < 100; b++ )
-    {
-      data.push( "x", binCenter( b ) );
-      data.push( "y", y[ b ], error( y[ b ] ) );
-    }
+  {
+    data.push( "x", binCenter( b ) );
+    data.push( "y", y[ b ], error( y[ b ] ) );
+  }
 
   return;
 }
@@ -55,60 +60,68 @@ void   readData ( std::string fileName, Dataset& data )
 
 int main( int argc, char** argv )
 {
-  // If working with MPI, initialize it.
-#ifdef MPI_ON
-  MPI::Init( argc, argv );
-  const int rank = MPI::COMM_WORLD.Get_rank();
-#else
-  const int rank = 0;
-#endif
-
-  // Data container.
-  Dataset data;
-
-  // Read the data. If working with MPI, only the root
-  //    process reads the data, then it gets scattered.
-  if ( rank == 0 )
-    readData( "data/gauss.dat", data );
-#ifdef MPI_ON
-  data.scatter();
-#endif
+  if ( argc < 2 )
+  {
+    std::cerr << "\nUsage: " << argv[ 0 ] << " [gen|fit]\n" << std::endl;
+    return 1;
+  }
 
   // Variables the model depends on.
   Variable x( "x" );
   Variable y( "y" );
 
   // Parameters of the model.
-  Parameter area  ( "area"  , 4.e5, 300. );
-  Parameter phi   ( "phi"   , 0.5 , 0.1  );
-//Parameter area1 ( "area1" , 2.e5, 300. );
-//Parameter area2 ( "area2" , 2.e5, 300. );
-  Parameter mean1 ( "mean1" , -1. , .003 ); 
-  Parameter mean2 ( "mean2" , 2.  , .003 );
-  Parameter sigma1( "sigma1", 2.  , .003 );
-  Parameter sigma2( "sigma2", 1.  , .003 );
+  Parameter area  ( "area"  ,  2.0e5, 300.0   );
+  Parameter phi   ( "phi"   ,  0.5  ,   0.1   );
+  Parameter mean1 ( "mean1" , -1.0  ,   0.003 );
+  Parameter mean2 ( "mean2" ,  2.0  ,   0.003 );
+  Parameter sigma1( "sigma1",  2.0  ,   0.003 );
+  Parameter sigma2( "sigma2",  1.0  ,   0.003 );
 
   // Definition of the pdf.
   Gauss g1( x, mean1, sigma1 );
   Gauss g2( x, mean2, sigma2 );
 
-//Pdf sum =                     pow( sin( phi ), 2 ) * g1 + pow( cos( phi ), 2 ) * g2;
-//Pdf sum =   area          * ( pow( sin( phi ), 2 ) * g1 + pow( cos( phi ), 2 ) * g2 );
-  Pdf sum = ( area + 4.e5 ) * ( pow( sin( phi ), 2 ) * g1 + pow( cos( phi ), 2 ) * g2 );
+  if ( std::string( argv[ 1 ] ) == "gen" )
+  {
+    Pdf sum = g1 + g2;
 
-  // Definition of the minimizer from the pdf.
-  Chi2 chi2( sum, y, data );
+    // Open the output file.
+    std::ofstream output( "data/gauss.dat" );
 
-  // Compute the minimum.
-  FunctionMinimum min = chi2.minimize();
+    // Fill the output file with randomly generated values.
+    std::map< std::string, double > entry;
+    for ( unsigned k = 0; k < 1000000; ++k )
+    {
+      entry = sum.generate();
+      for ( std::map< std::string, double >::const_iterator key = entry.begin(); key != entry.end(); ++key )
+        output << key->second << std::endl;
+    }
 
-  // If working with MPI, only the root process outputs the result.
-  if ( rank == 0 )
+    // Close the output file.
+    output.close();
+  }
+
+  if ( std::string( argv[ 1 ] ) == "fit" )
+  {
+    // Data container.
+    Dataset data;
+
+    // Read the data.
+    readData( "data/gauss.dat", data );
+
+    // Define the pdf to be fitted.
+    Pdf sum = area * ( pow( sin( phi ), 2 ) * g1 + pow( cos( phi ), 2 ) * g2 );
+
+    // Definition of the minimizer from the pdf.
+    Chi2 chi2( sum, y, data );
+
+    // Compute the minimum.
+    const FunctionMinimum& min = chi2.minimize();
+
+    // Output the result.
     std::cout << min << std::endl;
-
-#ifdef MPI_ON
-  MPI::Finalize();
-#endif
+  }
 
   return 0;
 }
