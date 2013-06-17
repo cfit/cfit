c3c6d0ddd8c8d511e605e368f2b8cecbce1f55e7
jordix <cfit@lavabit.com>
2013-06-17 12:07:06 +0200

    Add functions to evaluate the inverse of the erf function.

--- include/cfit/math.hh
+++ include/cfit/math.hh
@@ -4,14 +4,17 @@
 class Math
 {
 private:
-  static const double lngamma   (       double  x );
-  static const double gammastirf( const double& x );
+  static const double lngamma   (       double  x  );
+  static const double gammastirf( const double& x  );
+  static const double invnormal ( const double& y0 );
+
 public:
   static const double erf       ( const double& x );
   static const double erfc      ( const double& x );
   static const double gamma     (       double  x );
   static const double gamma_p   ( const double& a, const double& x );
   static const double gamma_q   ( const double& a, const double& x );
+  static const double inverf    ( const double& x );
 };
 
 
--- src/math.cc
+++ src/math.cc
@@ -1,5 +1,6 @@
 
 #include <cmath>
+#include <limits>
 #include <cfit/math.hh>
 
 
@@ -388,7 +389,6 @@ const double Math::lngamma( double x )
   double u;
   double w;
   double z;
-  unsigned i;
   double logpi;
   double ls2pi;
 
@@ -399,7 +399,6 @@ const double Math::lngamma( double x )
     q = -x;
     w = lngamma( q );
     p = std::floor( q );
-    i = (unsigned) std::floor( p + 0.5 );
     z = q-p;
     if( z > 0.5 )
     {
@@ -601,5 +600,162 @@ const double Math::erfc( const double& x )
   q = 4958.82756472114071495438422+x*q;
   q = 1826.3348842295112595576438+x*q;
   return std::exp( -std::pow( x, 2 ) )*p/q;
+}
+
+
+
+
+/*************************************************************************
+Inverse of the error function
+
+Cephes Math Library Release 2.8:  June, 2000
+Copyright 1984, 1987, 1988, 1992, 2000 by Stephen L. Moshier
+*************************************************************************/
+const double Math::inverf( const double& x )
+{
+  return invnormal( 0.5 * ( x + 1.0 ) ) / std::sqrt( 2.0 );
+}
+
+
+
+
+/*************************************************************************
+Inverse of Normal distribution function
+
+Returns the argument, x, for which the area under the
+Gaussian probability density function (integrated from
+minus infinity to x) is equal to y.
+
+
+For small arguments 0 < y < exp(-2), the program computes
+z = sqrt( -2.0 * log(y) );  then the approximation is
+x = z - log(z)/z  - (1/z) P(1/z) / Q(1/z).
+There are two rational functions P/Q, one for 0 < y < exp(-32)
+and the other for y up to exp(-2).  For larger arguments,
+w = y - 0.5, and  x/sqrt(2pi) = w + w**3 R(w**2)/S(w**2)).
+
+ACCURACY:
+
+                     Relative error:
+arithmetic   domain        # trials      peak         rms
+   IEEE     0.125, 1        20000       7.2e-16     1.3e-16
+   IEEE     3e-308, 0.135   50000       4.6e-16     9.8e-17
+
+Cephes Math Library Release 2.8:  June, 2000
+Copyright 1984, 1987, 1988, 1992, 2000 by Stephen L. Moshier
+*************************************************************************/
+const double Math::invnormal( const double& y0 )
+{
+  double expm2;
+  double s2pi;
+  double x;
+  double y;
+  double z;
+  double y2;
+  double x0;
+  double x1;
+  bool code;
+  double p0;
+  double q0;
+  double p1;
+  double q1;
+  double p2;
+  double q2;
+
+  expm2 = 0.13533528323661269189;
+  s2pi = 2.50662827463100050242;
+
+  if ( y0 <= 0.0 )
+    return -std::numeric_limits< double >::infinity();
+
+  if( y0 >= 1.0 )
+    return std::numeric_limits< double >::infinity();
+
+  code = true;
+  y = y0;
+
+  if( y > 1.0 - expm2 )
+  {
+    y = 1.0 - y;
+    code = false;
+  }
+
+  if( y > expm2 )
+  {
+    y = y-0.5;
+    y2 = y*y;
+    p0 = -59.9633501014107895267;
+    p0 = 98.0010754185999661536+y2*p0;
+    p0 = -56.6762857469070293439+y2*p0;
+    p0 = 13.9312609387279679503+y2*p0;
+    p0 = -1.23916583867381258016+y2*p0;
+    q0 = 1;
+    q0 = 1.95448858338141759834+y2*q0;
+    q0 = 4.67627912898881538453+y2*q0;
+    q0 = 86.3602421390890590575+y2*q0;
+    q0 = -225.462687854119370527+y2*q0;
+    q0 = 200.260212380060660359+y2*q0;
+    q0 = -82.0372256168333339912+y2*q0;
+    q0 = 15.9056225126211695515+y2*q0;
+    q0 = -1.18331621121330003142+y2*q0;
+    x = y+y*y2*p0/q0;
+    x = x*s2pi;
+
+    return x;
+  }
+
+  x = std::sqrt( -2.0 * std::log( y ) );
+  x0 = x - std::log( x ) / x;
+  z = 1.0/x;
+  if( x < 8.0 )
+  {
+    p1 = 4.05544892305962419923;
+    p1 = 31.5251094599893866154+z*p1;
+    p1 = 57.1628192246421288162+z*p1;
+    p1 = 44.0805073893200834700+z*p1;
+    p1 = 14.6849561928858024014+z*p1;
+    p1 = 2.18663306850790267539+z*p1;
+    p1 = -1.40256079171354495875*0.1+z*p1;
+    p1 = -3.50424626827848203418*0.01+z*p1;
+    p1 = -8.57456785154685413611*0.0001+z*p1;
+    q1 = 1;
+    q1 = 15.7799883256466749731+z*q1;
+    q1 = 45.3907635128879210584+z*q1;
+    q1 = 41.3172038254672030440+z*q1;
+    q1 = 15.0425385692907503408+z*q1;
+    q1 = 2.50464946208309415979+z*q1;
+    q1 = -1.42182922854787788574*0.1+z*q1;
+    q1 = -3.80806407691578277194*0.01+z*q1;
+    q1 = -9.33259480895457427372*0.0001+z*q1;
+    x1 = z*p1/q1;
+  }
+  else
+  {
+    p2 = 3.23774891776946035970;
+    p2 = 6.91522889068984211695+z*p2;
+    p2 = 3.93881025292474443415+z*p2;
+    p2 = 1.33303460815807542389+z*p2;
+    p2 = 2.01485389549179081538*0.1+z*p2;
+    p2 = 1.23716634817820021358*0.01+z*p2;
+    p2 = 3.01581553508235416007*0.0001+z*p2;
+    p2 = 2.65806974686737550832*0.000001+z*p2;
+    p2 = 6.23974539184983293730*0.000000001+z*p2;
+    q2 = 1;
+    q2 = 6.02427039364742014255+z*q2;
+    q2 = 3.67983563856160859403+z*q2;
+    q2 = 1.37702099489081330271+z*q2;
+    q2 = 2.16236993594496635890*0.1+z*q2;
+    q2 = 1.34204006088543189037*0.01+z*q2;
+    q2 = 3.28014464682127739104*0.0001+z*q2;
+    q2 = 2.89247864745380683936*0.000001+z*q2;
+    q2 = 6.79019408009981274425*0.000000001+z*q2;
+    x1 = z*p2/q2;
+  }
+  x = x0-x1;
+
+  if( code )
+    return -x;
+
+  return x;
 }
 
