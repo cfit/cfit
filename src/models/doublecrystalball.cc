a0149833b01dd072616a178c27080bbbdffdac51
jordix <cfit@lavabit.com>
2013-06-18 12:05:36 +0200

    Add generate function for the double Crystal Ball model.

--- src/models/doublecrystalball.cc
+++ src/models/doublecrystalball.cc
@@ -2,6 +2,8 @@
 #include <cfit/math.hh>
 #include <cfit/models/doublecrystalball.hh>
 
+#include <cfit/random.hh>
+
 DoubleCrystalBall::DoubleCrystalBall( const Variable& x,
                                       const Parameter& mu   , const Parameter& sigma,
                                       const Parameter& alpha, const Parameter& n,
@@ -280,4 +282,54 @@ const double DoubleCrystalBall::area( const double& min, const double& max ) con
 {
   return ( cumulativeNorm( max ) - cumulativeNorm( min ) ) / _norm;
 }
+
+
+
+const std::map< std::string, double > DoubleCrystalBall::generate() const throw( PdfException )
+{
+  const double& vn     = n();
+  const double& vnm1   = vn - 1.0;
+  const double& vm     = m();
+  const double& vmm1   = vm - 1.0;
+  const double& vsigma = sigma();
+  const double& valpha = alpha();
+  const double& vbeta  = beta();
+  const double& sqrt2  = std::sqrt( 2.0 );
+
+  // Evaluate the area up to the lower limit (0 if it's -infinity).
+  double areaLo = 0.0;
+  if ( _hasLower )
+    areaLo = cumulativeNorm( _lower ) / _norm;
+
+  // Cumulative pdf evaluated at the tail-core threshold.
+  const double& areaTh1 = vsigma / ( valpha * _norm ) * vn / vnm1 * std::exp( - std::pow( valpha, 2 ) / 2.0 );
+  const double& areaTh2 = areaTh1 + vsigma / _norm * std::sqrt( M_PI / 2.0 ) * ( std::erf( valpha / sqrt2 ) + std::erf( vbeta / sqrt2 ) );
+
+  // Useful factor used in the generation of numbers in the upper tail.
+  const double& coefHi = vsigma / ( vbeta * _norm ) * vm / vmm1 * std::exp( - std::pow( vbeta, 2 ) / 2.0 );
+
+  // Generate a flat random number.
+  std::uniform_real_distribution< double > dist( 0.0, 1.0 );
+  const double& unif = dist( Random::engine() ) + areaLo;
+
+  double genVal = 0.0;
+
+  // If random number is below the lower tail-core threshold, generate the lower tail.
+  //    If it's above the upper tail-core threshold, generate the upper tail.
+  //    Otherwise, generate the (Gaussian) core.
+  if ( unif < areaTh1 )
+    genVal = mu() - valpha * vsigma - vn * vsigma / valpha * ( std::pow( areaTh1 / unif                      , 1.0 / vnm1 ) - 1.0 );
+  else if ( unif > areaTh2 )
+    genVal = mu() + vbeta  * vsigma + vm * vsigma / vbeta  * ( std::pow( coefHi / ( areaTh2 + coefHi - unif ), 1.0 / vmm1 ) - 1.0 );
+  else
+    genVal = mu() + vsigma * sqrt2 * Math::inverf( _norm / vsigma * std::sqrt( 2.0 / M_PI ) * ( unif - areaTh1 ) -
+                                                   std::erf( valpha / sqrt2 ) );
+
+  // Fill the map with the generated value.
+  std::map< std::string, double > gen;
+  gen[ getVar( 0 ).name() ] = genVal;
+
+  return gen;
+}
+
 
