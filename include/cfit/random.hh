a0bbe63b0fb3b7a17964a75c3a1c3059f50fae11
jordix <cfit@lavabit.com>
2013-08-06 15:43:59 +0200

    Add functions to change the random seed and to generate gaussian random numbers.

--- include/cfit/random.hh
+++ include/cfit/random.hh
@@ -6,24 +6,32 @@
 class Random
 {
 private:
+  static std::default_random_engine               _engine;
+
   static std::uniform_real_distribution< double > _uniform;
-  static std::default_random_engine _e;
+  static std::normal_distribution      < double > _normal;
 
 public:
   static std::default_random_engine& engine()
   {
-    return _e;
+    return _engine;
   }
 
-  static void setSeed( const unsigned& seed ) { _e.seed( seed ); }
+  static void setSeed( const unsigned& seed ) { _engine.seed( seed ); }
 
   static const double flat   ( const double& min = 0.0, const double& max = 1.0 )
   {
     return min + ( max - min ) * _uniform( engine() );
   }
+
   static const double uniform( const double& min = 0.0, const double& max = 1.0 )
   {
     return min + ( max - min ) * _uniform( engine() );
+  }
+
+  static const double normal( const double& mu = 0.0, const double& sigma = 1.0 )
+  {
+    return mu + sigma * _normal( _engine );
   }
 };
 
--- src/random.cc
+++ src/random.cc
@@ -1,7 +1,8 @@
 
 #include <cfit/random.hh>
 
-std::uniform_real_distribution< double > Random::_uniform = std::uniform_real_distribution< double >();
+std::default_random_engine Random::_engine = std::default_random_engine();
 
-std::default_random_engine Random::_e = std::default_random_engine();
+std::uniform_real_distribution< double > Random::_uniform = std::uniform_real_distribution< double >();
+std::normal_distribution      < double > Random::_normal  = std::normal_distribution      < double >();
 
